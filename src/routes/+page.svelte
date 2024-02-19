42d139de7f19247a2863c1486179657e1fb0fbd6
Milthon Oppman Hellhoff <milopp@elevad.umea.se>
2024-02-19 12:04:37 +0100

    Klar med Start

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,37 @@
+<script>
+	let name = 'Miller';
+  let toa = 'apa';
+</script>
+
+
 
 <h1>
-  Välkommen till min hemsida!
+  Välkomen till min hemsida! Milthononoon
 </h1>
 
 <h2>Här har jag samlat min uppgifter jag gjort i samband med webbutveckling i årskurs 2 på Maja Beskowgymnasiet!</h2>
 
-<h3 >Förnamn Efternamn</h3>
+<h4> Hej! {name} och {toa}</h4>
+
+<h3
+class="hej">Förnamn Efternamn</h3>
+
+<p style = 'color:cadetblue; font-size: 2em'>
+  ank
+  </p>
+
+<style>
+  h1{
+    color:green;
+  }
+  h2{
+    color:blue
+  }
+  .hej{
+  color: rebeccapurple;
+  font-family:'Gill Sans'
+  }
+</style>
+
+
 
