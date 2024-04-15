d2da360b0bf94e249af1e4692cb0aa067e35b032
Milthon Oppman Hellhoff <milopp@elevad.umea.se>
2024-04-15 10:21:44 +0200

    klar med memori

--- src/routes/memory/+page.svelte
+++ src/routes/memory/+page.svelte
@@ -1,36 +1,81 @@
 <script>
   let cards = [];
+  let pictures = []
+  let flipped_cards = []
+  let images_number = 6
+  let blå = 0
+  let röd = 0
+  let blueturn = true
+  
+  for(let i = 0; i< images_number; i++){
+    pictures.push("https://picsum.photos/200/200?random"+i.toString())
+  }
+
   for (let index = 0; index < 12; index++) {
     cards.push({
-      id: 5, // TODO: unique ids per card card
-      img: "/notrick.gif", // TODO: unique images per card card
+      id: index,
+      img: pictures[index%images_number], // TODO: unique images per card card
       flipped: false,  // TODO: think
       completed: false,
     });
   }
-  let flipcount = 0;
+
+  function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+shuffleArray(cards)
+  
+let flipcount = 0;
+
+
   function flip(card) {
     // flip card over if two cards are not already flipped
     // TODO: and card is already not flipped
-    if (card.flipped && flipcount < 2) {
+    if (!card.flipped && flipcount < 2) {
       // TODO: Probably do what?
-
+      card.flipped = true
+      flipcount ++
+      flipped_cards.push(card.img)
+    
       // flip the cards over after 2s from seeing both cards
-      if (flipcount == 4) {
+      if (flipcount == 2) {
+        if(flipped_cards[0] === flipped_cards[1]){
+          console.log('samma kort')
+          cards.forEach((card) => {
+            if(card.flipped){card.completed = true}
+        });
+        flipcount = 0
+        flipped_cards = []
+        if(blueturn){blå++}
+        else {röd ++}
+      }
+        
         setTimeout(() => {
           // flip over cards that have not been marked as "completed"
           cards.forEach((card) => {
             card.flipped = card.completed;
+                console.log('vänder korten') 
+                console.log(card.completed)
+            cards=cards
           });
-          flipcount = 0;
-          cards = cards;
-        }, 2000);
+          flipcount = 0
+          flipped_cards = []
+          blueturn = !blueturn
+          }, 1000);
+
       }
-      cards = cards;
-    } else {
-      alert("chilla, ta de koool");
-    }
+
+    } 
+    cards = cards;
   }
+
+
 </script>
 
 <main>
@@ -51,9 +96,21 @@
       </div>
     {/each}
   </div>
-  <div class="red-box"></div>
-  <div class="blue-box"></div>
+  <div class = "box" id = "red-box">
+    <p>
+      {röd}
+    </p>
+  </div>
+  <div class= 'box' id = "blue-box">
+    <p>
+      {blå}
+    </p>
+  </div>
+  <div class = 'box' id = 'turn-box' style={blueturn?"right: 10px;":"left:10px"}>
+  </div>
 </main>
+<div style="width:600px; height:800px; background-
+color:bisque;"></div>
 
 <style>
   main {
@@ -116,17 +173,35 @@
     -webkit-backface-visibility: hidden;
     position: absolute;
   }
-  .red-box, .blue-box {
+  .box {
 width: 100px;
 height: 100px;
-position: absolute;
-bottom : 0;
+position: fixed;
+text-align: center;
+font-size: 30px;
+}  
+  #red-box, #blue-box {
+z-index: 2;
+bottom: 0;
 }
-.red-box {
+#red-box {
 background-color: red;
-z-index:1;
+z-index:2;
+left: 0px;
+text-align: center; font-size: 30px;
 }
-.blue-box {
+#blue-box {
 background-color: blue;
+z-index: 2;
+Right: 0px;
+text-align: center; font-size: 30px;
+
 }
+#turn-box{
+bottom: 10px;
+z-index: 1;
+background-color: greenyellow;
+box-shadow: 0 0 20px 20px greenyellow;
+}
+
 </style>
